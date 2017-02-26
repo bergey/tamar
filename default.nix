20ccefacbe54c9434e1d3adbe7cbdb8804e553db
Daniel Bergey <bergey@teallabs.org>
2017-02-26 12:47:54 -0500

    delete errant default.nix from merge

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ stdenv, SDL2, cairo, pkgconfig, pango, alex, ghc, cabal-install }:
-
-stdenv.mkDerivation {
-  name = "sdl-pango";
-  # builder = ./builder.sh;
-  buildInputs = [ SDL2 cairo pkgconfig pango alex ghc cabal-install ];
-  src = ./.;
-}
